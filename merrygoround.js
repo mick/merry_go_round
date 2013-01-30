ea774f63dd58ef366014d1a156b1ec43fd3b3920
Mick Thompson <dthompson@gmail.com>
2013-01-29 16:05:31 -0800

    initial commit

---
+++ merrygoround.js
@@ -0,0 +1,43 @@
+$(function(){
+
+
+
+
+// TODO: make this a param
+var sitesUrl = "/sites.json";
+var timing = 5000;
+
+var sites = [], currentSite = 0;
+
+var rotateSite = function(){
+  $("iframe#site-"+currentSite).hide();
+
+  if(currentSite+1 < sites.length)
+    currentSite++;
+  else
+    currentSite = 0;
+  
+  $("iframe#site-"+currentSite).show();
+  setTimeout(rotateSite, timing);
+};
+
+
+$.ajax(sitesUrl,{dataType:"json", success:function(data){
+  sites = data.sites;
+  data.sites.forEach(function(s, i){
+    $("body").append("<iframe id='site-"+i+"' border='0' class='site' src='"+s.url+"'></iframe>");
+  });
+  setTimeout(rotateSite, timing);
+
+},
+error: function(err){
+
+  $("body").html("Error loading sites json file from:  "+sitesUrl);
+
+}});
+
+
+
+
+
+});
